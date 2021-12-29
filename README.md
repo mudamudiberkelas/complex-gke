3c7f411a0ca37fcb0a2a61d5c9d5280aeac25013
mudamudiberkelas <96584486+mudamudiberkelas@users.noreply.github.com>
2021-12-29 10:54:10 +0700

    yaml

--- README.md
+++ README.md
@@ -3,17 +3,17 @@ docker build -t mudamudiberkelas/react-test -f ./client/Dockerfile.dev ./client
 docker run -e CI=true mudamudiberkelas/react-test npm test
 
 # Docker Build
-docker build -t mudamudiberkelas/multi-client ./client
+docker build -t mudamudiberkelas/multi-client-k8s:latest ./client
 docker build -t mudamudiberkelas/multi-nginx ./nginx
-docker build -t mudamudiberkelas/multi-server ./server
-docker build -t mudamudiberkelas/multi-worker ./worker
+docker build -t mudamudiberkelas/multi-server-k8s-pgfix:latest ./server
+docker build -t mudamudiberkelas/multi-worker-k8s:latest ./worker
 
 # Docker Login
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
 
 # Take those images and push them to docker hub
-docker push mudamudiberkelas/multi-client
+docker push mudamudiberkelas/multi-client-k8s:latest
 docker push mudamudiberkelas/multi-nginx
-docker push mudamudiberkelas/multi-server
-docker push mudamudiberkelas/multi-worker
+docker push mudamudiberkelas/multi-server-k8s-pgfix:latest
+docker push mudamudiberkelas/multi-worker-k8s:latest
 
--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t mudamudiberkelas/multi-client-k8s:latest -t mudamudiberkelas/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
-docker build -t mudamudiberkelas/multi-server-k8s-pgfix:latest -t mudamudiberkelas/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
-docker build -t mudamudiberkelas/multi-worker-k8s:latest -t mudamudiberkelas/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
+docker build -t mudamudiberkelas/multi-client-k8s:latest-k8s:latest -t mudamudiberkelas/multi-client-k8s:latest-k8s:$SHA -f ./client/Dockerfile ./client
+docker build -t mudamudiberkelas/multi-server-k8s-pgfix:latest -t mudamudiberkelas/multi-server-k8s-pgfix:latest-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
+docker build -t mudamudiberkelas/multi-worker-k8s:latest-k8s:latest -t mudamudiberkelas/multi-worker-k8s:latest-k8s:$SHA -f ./worker/Dockerfile ./worker
 
-docker push mudamudiberkelas/multi-client-k8s:latest
+docker push mudamudiberkelas/multi-client-k8s:latest-k8s:latest
 docker push mudamudiberkelas/multi-server-k8s-pgfix:latest
-docker push mudamudiberkelas/multi-worker-k8s:latest
+docker push mudamudiberkelas/multi-worker-k8s:latest-k8s:latest
 
-docker push mudamudiberkelas/multi-client-k8s:$SHA
-docker push mudamudiberkelas/multi-server-k8s-pgfix:$SHA
-docker push mudamudiberkelas/multi-worker-k8s:$SHA
+docker push mudamudiberkelas/multi-client-k8s:latest-k8s:$SHA
+docker push mudamudiberkelas/multi-server-k8s-pgfix:latest-k8s-pgfix:$SHA
+docker push mudamudiberkelas/multi-worker-k8s:latest-k8s:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=mudamudiberkelas/multi-server-k8s-pgfix:$SHA
-kubectl set image deployments/client-deployment client=mudamudiberkelas/multi-client-k8s:$SHA
-kubectl set image deployments/worker-deployment worker=mudamudiberkelas/multi-worker-k8s:$SHA
+kubectl set image deployments/server-deployment server=mudamudiberkelas/multi-server-k8s-pgfix:latest-k8s-pgfix:$SHA
+kubectl set image deployments/client-deployment client=mudamudiberkelas/multi-client-k8s:latest-k8s:$SHA
+kubectl set image deployments/worker-deployment worker=mudamudiberkelas/multi-worker-k8s:latest-k8s:$SHA
